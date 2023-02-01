95dc479caeb8513a94586614dd51ab3890ea231f
Ahmed laghrissi <99613580+laghri@users.noreply.github.com>
2023-02-01 22:01:30 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__, static_folder='static')
+
+model = pickle.load(open('Credit_Model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html' , condition=output)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
